ec7b56078e03a2c19978c1d6523fb3dbbae0070d
Jacqueline Yau <jyau@stanford.edu>
2021-03-09 19:49:18 -0800

    added script to extract the 3d images and save them as individual files

---
+++ scripts/get_3d_matrix_from_pickle.py
@@ -0,0 +1,32 @@
+import pickle as pkl
+import os
+import numpy as np
+from tempfile import TemporaryFile
+from glob import glob
+
+PICKLE_PATH = ['data/containers/shelves/normal/placements/', "data/containers/fridges/12252/placements/"]
+# Load the output files with np.load(file)
+# More info here: https://numpy.org/doc/stable/reference/generated/numpy.save.html
+
+# Output directory
+if not os.path.isdir('./imgs_3d/'):
+  os.mkdir('./imgs_3d/')
+
+# example: data/containers/fridges/12252/placements/shelf_0/Fruit/shelf_setup_4.pkl
+for placement in PICKLE_PATH:
+  shelves = os.listdir(placement)
+  for shelf in shelves:
+  	objects = os.listdir(os.path.join(PICKLE_PATH, shelf))
+  	for obj in objects:
+  		files = glob(os.path.join(os.path.join(PICKLE_PATH, shelf), obj) + '*.pkl')
+  		for file in files:
+  			with open(file, 'rb') as out:
+          p = pkl.load(out)
+          keys = p.keys()
+          for key in keys:
+    				# outfile = TemporaryFile()
+            title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_im3d'
+    				np.save(title, p[key]['im3d'])
+
+            title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_depth_m3d'
+            np.save('./imgs_3d/'+title, p[key]['depth_im3d'])
