78b9d993a63ab91a8805971881eff241e5da52f9
jhyau <warplaneb3@gmail.com>
2021-03-10 04:11:50 +0000

    fixed the script to extract the 3d img matrices

--- scripts/get_3d_matrix_from_pickle.py
+++ scripts/get_3d_matrix_from_pickle.py
@@ -16,17 +16,22 @@ if not os.path.isdir('./imgs_3d/'):
 for placement in PICKLE_PATH:
   shelves = os.listdir(placement)
   for shelf in shelves:
-    objects = os.listdir(os.path.join(PICKLE_PATH, shelf))
+    objects = os.listdir(os.path.join(placement, shelf))
     for obj in objects:
-      files = glob(os.path.join(os.path.join(PICKLE_PATH, shelf), obj) + '*.pkl')
+      path = os.path.join(os.path.join(placement, shelf), obj)
+      files = glob(os.path.join(os.path.join(placement, shelf), obj) + '/*.pkl')
       for file in files:
         with open(file, 'rb') as out:
           p = pkl.load(out)
           keys = p.keys()
           for key in keys:
-        		# outfile = TemporaryFile()
+            print(f'{p[key].keys()}')
+            if 'im3d' not in p[key].keys():
+              continue
+            # outfile = TemporaryFile()
             title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_im3d'
-            np.save(title, p[key]['im3d'])
+            print(f'{title}')
+            np.save('./imgs_3d/'+title, p[key]['im3d'])
 
-            title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_depth_m3d'
+            title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_depth_im3d'
             np.save('./imgs_3d/'+title, p[key]['depth_im3d'])
