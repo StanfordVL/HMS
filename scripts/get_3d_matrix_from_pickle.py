e829fb28ee04a495d6ec62f1ac3e66de7bd466ab
jhyau <warplaneb3@gmail.com>
2021-03-11 16:46:15 +0000

    fixed script to save more things, and in an orderly fashion

--- scripts/get_3d_matrix_from_pickle.py
+++ scripts/get_3d_matrix_from_pickle.py
@@ -3,8 +3,9 @@ import os
 import numpy as np
 from tempfile import TemporaryFile
 from glob import glob
+from pathlib import Path
 
-PICKLE_PATH = ['data/containers/shelves/normal/placements/', "data/containers/fridges/12252/placements/"]
+PICKLE_PATH = ['data/containers/shelves/normal/placements/'] #"data/containers/fridges/12252/placements/"]
 # Load the output files with np.load(file)
 # More info here: https://numpy.org/doc/stable/reference/generated/numpy.save.html
 
@@ -20,21 +21,23 @@ for placement in PICKLE_PATH:
   for shelf in shelves:
     objects = os.listdir(os.path.join(placement, shelf))
     for obj in objects:
-      path = os.path.join(os.path.join(placement, shelf), obj)
+      #path = os.path.join(os.path.join(placement, shelf), obj)
       files = glob(os.path.join(os.path.join(placement, shelf), obj) + '/*.pkl')
       for file in files:
-        path = os.path.join(path, file.split('/')[-1].split('.')[0])
+        out_path = os.path.join(os.path.join(os.path.join('./imgs_3d/', shelf), obj), file.split('/')[-1].split('.')[0])
         with open(file, 'rb') as out:
           p = pkl.load(out)
           keys = p.keys()
           for key in keys:
-            path = os.path.join(path, str(key))
-            print(f'{p[key].keys()}')
+            path = os.path.join(out_path, str(key))
+            #print(f'{p[key].keys()}')
             if 'im3d' not in p[key].keys():
               continue
             # outfile = TemporaryFile()
             title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_im3d'
             print(f'{title}')
+            print(f'{path}')
+            Path(path).mkdir(parents=True, exist_ok=True)
             np.save(path+'/'+title, p[key]['im3d'])
 
             title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_depth_im3d'
@@ -45,3 +48,4 @@ for placement in PICKLE_PATH:
 
             title = shelf + '_' + obj + '_' + file.split('/')[-1].split('.')[0] + '_' + str(key) + '_camera_intrinsics'
             np.save(path+'/'+title, p[key]['K'])
+
